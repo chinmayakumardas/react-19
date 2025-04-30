6f49271d9c8e845a9637d3cf39660536b0e6c49b
chinmayakumardas <chinmayakumardas2000@gmail.com>
2025-04-30 07:48:33 +0530

    login added

---
+++ 1-Installation-setup/demo/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import LoginPage from './Login'
+
+function App() {
+
+
+  return (
+    <div className="min-h-screen">
+      <LoginPage />
+  
+    </div>
+  )
+}
+
+export default App
