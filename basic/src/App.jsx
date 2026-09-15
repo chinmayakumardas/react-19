5e4dc86e207311031b60d03805c8e1a34d7b1ab2
CHINMAYA KUMAR DAS <chinmayakumardas2000@gmail.com>
2026-09-15 22:38:21 +0530

    ok

--- basic/src/App.jsx
+++ basic/src/App.jsx
@@ -10,16 +10,11 @@ function App() {
   return (
     <>
       <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
+     
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
-        </div>
+    
         <button
           type="button"
           className="counter"
